a63778f6637531892b98291a1968508c53308aea
Ranosaurus07 <vx.comimran@yahoo.com>
2023-04-15 14:00:56 +0800

    Control dan placeholder sprite

--- ASAP - Nang Ka/Assets/Script/Player Script/Player.cs
+++ ASAP - Nang Ka/Assets/Script/Player Script/Player.cs
@@ -11,9 +11,13 @@ public class Player : MonoBehaviour
 
     public Animator Anim { get; private set; }
     public PlayerInputHandler InputHandler { get; private set; }  
+    public Rigidbody2D RB { get; private set; }
+    public Vector2 CurrentVelocity { get; private set; }
 
     [SerializeField]
     private PlayerData playerData;
+
+    private Vector2 workspace;
 
     private void Awake()
     {
@@ -27,17 +31,26 @@ public class Player : MonoBehaviour
     {
         Anim = GetComponent<Animator>();
         InputHandler = GetComponent<PlayerInputHandler>();
+        RB = GetComponent<Rigidbody2D>();
 
         StateMachine.Initialize(IdleState);
     }
 
     private void Update()
     {
+        CurrentVelocity = RB.velocity;
         StateMachine.CurrentState.LogicUpdate();
     }
 
     private void FixedUpdate()
     {
         StateMachine.CurrentState.PhysicsUpdate();
+    }
+
+    public void SetVelocityX(float velocity)
+    {
+        workspace.Set(velocity, CurrentVelocity.y);
+        RB.velocity = workspace;
+        CurrentVelocity = workspace;
     }
 }
