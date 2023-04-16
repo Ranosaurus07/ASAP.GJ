749221716007963a9785dbe965be65e5af16286a
ImSadnessinDisguise <130771060+ImSadnessinDisguise@users.noreply.github.com>
2023-04-17 00:30:26 +0800

    try meta

---
+++ ASAP - Nang Ka/Assets/Enemy/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float walkSpeed = 3f;
+
+    Rigidbody2D rb;
+
+
+    
+  
+}
